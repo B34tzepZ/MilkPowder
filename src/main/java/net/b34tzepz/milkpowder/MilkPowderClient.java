ef2ec3dd31b56d7637345e69f9ff2ffc829c123d
B34tzepZ <florian.graeber@gmx.de>
2025-08-19 02:15:51 +0200

    Initial commit

---
+++ src/main/java/net/b34tzepz/milkpowder/MilkPowderClient.java
@@ -0,0 +1,10 @@
+package net.b34tzepz.milkpowder;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MilkPowderClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
